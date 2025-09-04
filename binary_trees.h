6face693fb2221e4c5c6bc72c4c335296f981a14
Affracs <alexfrancois1501@gmail.com>
2025-09-04 07:37:29 +0000

    editted header file according to project's instruction

--- binary_trees.h
+++ binary_trees.h
@@ -1,8 +1,14 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
-#include <stddef.h>
-
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
 struct binary_tree_s
 {
     int n;
@@ -12,6 +18,13 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
+
+/**
+ *prototype declarations
+ */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
