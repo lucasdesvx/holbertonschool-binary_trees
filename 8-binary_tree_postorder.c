a06465a3ea0d6167d894482c4a7a719aaf39b45e
Your Name <you@example.com>
2026-01-11 21:09:21 +0000

    m

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,10 +1,11 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - goes through a binary tree using post-order traversal
- * @tree: pointer to the root node of the tree to traverse
+ * binary_tree_postorder - goes through a binary tree
+ * @tree: pointer to the root node of the tree
  * @func: function to call for each node
  */
+
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
